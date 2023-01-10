955a675f109881b5b949c9bcd84f8f8cc713a061
Emanuela91 <emanuela.marson91@gmail.com>
2023-01-10 18:06:27 +0100

    scritto il paragrafo, trovata la lunghezza e stampato a schermo

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BadWords</title>
+
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <p>
+        <?php
+        // Paragrafo "lorem"
+        $phar = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae in quo dolore? Adipisci a amet doloremque beatae. Repellendus a accusantium quibusdam hic eaque corrupti aut. Exercitationem non perferendis et consequuntur.";
+        // trovo la lunghezza del paragrafo
+        $lng = strlen($phar);
+        // lo stampo
+        echo $phar . " : " . $lng;
+        ?>
+    </p>
+
+</body>
+
+</html>
